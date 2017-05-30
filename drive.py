f18b5d6f962c445775c2dcaa75adbf9be4f69460
Yazeed Alrubyli <yazid.alrubyli@gmail.com>
2017-05-30 05:24:54 +0300

    Update drive.py

--- drive.py
+++ drive.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import RPi.GPIO as GPIO
 from time import sleep
 import curses
@@ -22,48 +24,55 @@ MotorBack1 = 37
 MotorBack2 = 35
 MotorBack = 33
 
-class Controller():
-	def __init__(self):
-        	GPIO.setmode(GPIO.BOARD)
 
-        	GPIO.setup(MotorFront1,GPIO.OUT)
-	        GPIO.setup(MotorFront2,GPIO.OUT)
-        	GPIO.setup(MotorFront,GPIO.OUT)
+class Controller:
 
-	        GPIO.setup(MotorBack1,GPIO.OUT)
-	        GPIO.setup(MotorBack2,GPIO.OUT)
-	        GPIO.setup(MotorBack,GPIO.OUT)
-	
-	def front(self,f1,f2,f):
-        	GPIO.output(MotorFront1,f1)
-        	GPIO.output(MotorFront2,f2)
-        	GPIO.output(MotorFront,f)
-		sleep(0.1)
-		GPIO.output(MotorFront,0)
+    def __init__(self):
+        GPIO.setmode(GPIO.BOARD)
 
-	def rear(self,b1,b2,b):
-        	GPIO.output(MotorBack1,b1)
-        	GPIO.output(MotorBack2,b2)
-        	GPIO.output(MotorBack,b)
-		sleep(0.1)
-		GPIO.output(MotorBack,0)
+        GPIO.setup(MotorFront1, GPIO.OUT)
+        GPIO.setup(MotorFront2, GPIO.OUT)
+        GPIO.setup(MotorFront, GPIO.OUT)
 
-if __name__ == "__main__":
-	carCtrl = Controller()
-	try:
-		while True:
-			char = screen.getch()
-			if char == ord('d'):
-				carCtrl.front(0,1,1)		
-			elif char == ord('a'):
-				carCtrl.front(1,0,1)			
-			if char == ord('w'):
-				carCtrl.rear(0,1,1)
-			elif char == ord('s'):
-				carCtrl.rear(1,0,1)
-			if char == ord(' '):
-				carCtrl.rear(0,0,0)
-	except:
-		curses.nocbreak(); screen.keypad(0); curses.echo()
-		curses.endwin()
-		GPIO.cleanup()
+        GPIO.setup(MotorBack1, GPIO.OUT)
+        GPIO.setup(MotorBack2, GPIO.OUT)
+        GPIO.setup(MotorBack, GPIO.OUT)
+
+    def front(self,f1,f2,f):
+        GPIO.output(MotorFront1, f1)
+        GPIO.output(MotorFront2, f2)
+        GPIO.output(MotorFront, f)
+        sleep(0.1)
+        GPIO.output(MotorFront, 0)
+
+    def rear(self,b1,b2,b):
+        GPIO.output(MotorBack1, b1)
+        GPIO.output(MotorBack2, b2)
+        GPIO.output(MotorBack, b)
+        sleep(0.1)
+        GPIO.output(MotorBack, 0)
+
+
+if __name__ == '__main__':
+    carCtrl = Controller()
+    try:
+        while True:
+            char = screen.getch()
+            if char == ord('d'):
+                carCtrl.front(0, 1, 1)
+            elif char == ord('a'):
+                carCtrl.front(1, 0, 1)
+            if char == ord('w'):
+                carCtrl.rear(0, 1, 1)
+            elif char == ord('s'):
+                carCtrl.rear(1, 0, 1)
+            if char == ord(' '):
+                carCtrl.rear(0, 0, 0)
+    except:
+        curses.nocbreak()
+        screen.keypad(0)
+        curses.echo()
+        curses.endwin()
+        GPIO.cleanup()
+
+			
