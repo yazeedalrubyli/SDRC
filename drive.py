88c0bbd4cf1a90bcbf2ebab7b72b1b9e1addccfa
Yazeed Alrubyli <yazeed.alrubyli@gmail.com>
2017-05-30 04:38:18 +0000

    Update drive.py

--- drive.py
+++ drive.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
+
 import RPi.GPIO as GPIO
 from time import sleep
 import curses
 
-# get the curses screen window
 screen = curses.initscr()
-
-# turn off input echoing
 curses.noecho()
-
-# respond to keys immediately (don't wait for enter)
 curses.cbreak()
-
-# map arrow keys to special values
 screen.keypad(True)
 
 MotorFront1 = 36
@@ -33,24 +26,22 @@ class Controller:
         GPIO.setup(MotorFront1, GPIO.OUT)
         GPIO.setup(MotorFront2, GPIO.OUT)
         GPIO.setup(MotorFront, GPIO.OUT)
+        GPIO.output(MotorFront, 0)
 
         GPIO.setup(MotorBack1, GPIO.OUT)
         GPIO.setup(MotorBack2, GPIO.OUT)
         GPIO.setup(MotorBack, GPIO.OUT)
+        GPIO.output(MotorBack, 0)
 
     def front(self,f1,f2,f):
         GPIO.output(MotorFront1, f1)
         GPIO.output(MotorFront2, f2)
         GPIO.output(MotorFront, f)
-        sleep(0.1)
-        GPIO.output(MotorFront, 0)
 
     def rear(self,b1,b2,b):
         GPIO.output(MotorBack1, b1)
         GPIO.output(MotorBack2, b2)
         GPIO.output(MotorBack, b)
-        sleep(0.1)
-        GPIO.output(MotorBack, 0)
 
 
 if __name__ == '__main__':
@@ -66,13 +57,13 @@ if __name__ == '__main__':
                 carCtrl.rear(0, 1, 1)
             elif char == ord('s'):
                 carCtrl.rear(1, 0, 1)
-            if char == ord(' '):
+                sleep(0.05)
                 carCtrl.rear(0, 0, 0)
+            if char == ord(' '):
+                carCtrl.front(0, 0, 0)
     except:
         curses.nocbreak()
         screen.keypad(0)
         curses.echo()
         curses.endwin()
         GPIO.cleanup()
-
-			
