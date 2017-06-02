5a2be1ff885d7cf65188e12f836a458aebccc18a
Yazeed Alrubyli <yazeed.alrubyli@gmail.com>
2017-06-02 04:39:36 +0000

    Update drive.py

--- drive.py
+++ drive.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
 from time import sleep
-import curses
-import csv
-import picamera
-import picamera.array
 from PIL import Image
-import os
+import picamera.array
+import picamera
 import thread
+import curses
+#import sys
+import csv
+import os
 
 screen = curses.initscr()
 curses.noecho()
@@ -20,11 +21,11 @@ class Camera:
     def __init__(self):
         self.camera = picamera.PiCamera()
         self.camera.vflip = True
-        self.stream = picamera.array.PiYUVArray(self.camera)
+        self.stream = picamera.array.PiRGBArray(self.camera)
     
     def capture(self):
-        self.camera.capture(self.stream, format='yuv')
-        image = self.stream.array[200:,:, 0]
+        self.camera.capture(self.stream, format='rgb')
+        image = self.stream.array[200:,:,:]
         self.stream.seek(0)
         self.stream.truncate()
         im = Image.fromarray(image)
@@ -44,6 +45,7 @@ class Controller:
     
     def __init__(self):
         GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
 
         GPIO.setup(self.MotorFront1, GPIO.OUT)
         GPIO.setup(self.MotorFront2, GPIO.OUT)
@@ -54,8 +56,12 @@ class Controller:
         GPIO.setup(self.MotorBack2, GPIO.OUT)
         GPIO.setup(self.MotorBack, GPIO.OUT)
         GPIO.output(self.MotorBack, 0)
+        self.BackPWM = GPIO.PWM(self.MotorBack,100)
+        self.BackPWM.start(0)
+        self.BackPWM.ChangeDutyCycle(0)
         
         self.direction = 0
+    
     def front(self,f1,f2,f):
         GPIO.output(self.MotorFront1, f1)
         GPIO.output(self.MotorFront2, f2)
@@ -64,10 +70,10 @@ class Controller:
     def rear(self,b1,b2,b):
         GPIO.output(self.MotorBack1, b1)
         GPIO.output(self.MotorBack2, b2)
-        GPIO.output(self.MotorBack, b)
+        self.BackPWM.ChangeDutyCycle(b)
     
     def steering(self):
-        while 1:
+        while True:
             char = screen.getch()
             if char == ord('d'):
                 self.front(0, 1, 1)
@@ -79,16 +85,17 @@ class Controller:
                 self.front(0, 0, 0)
                 self.direction = 0 
             if char == ord('w'):
-                self.rear(0, 1, 1)
+                self.rear(0, 1, 50)
             if char == ord(' '):
                 self.rear(1, 0, 1)
                 sleep(0.1)
                 self.rear(0, 0, 0)
+                #sys.exit()
 
 class Collector:
 
     def __init__(self, fileName='data/steering.csv'):
-        file = open(fileName, 'w')
+        file = open(fileName, 'a')
         self.writer = csv.writer(file)
 
     def write(self, direction):
@@ -101,7 +108,6 @@ if __name__ == '__main__':
     try:
         thread.start_new_thread(carCtrl.steering,())
         while True:
-            #thread.start_new_thread(carCtrl.steering,(carCtrl, direction))
             carCam.capture()
             carCol.write(str(carCtrl.direction))
     except:
@@ -109,5 +115,6 @@ if __name__ == '__main__':
         screen.keypad(0)
         curses.echo()
         curses.endwin()
+        carCtrl.BackPWM.stop()
         GPIO.cleanup()
         raise
